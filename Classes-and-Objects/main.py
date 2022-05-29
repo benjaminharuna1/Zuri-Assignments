e568d43cfa68f5e7c04f2dc08cc75b35b3ca37c7
benjaminharuna1 <105396700+benjaminharuna1@users.noreply.github.com>
2022-05-29 11:50:19 -0600

    Update main.py

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -3,30 +3,38 @@ from ast import Param
 
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self,name,age,tracks,score):
-        self.name, self.age, self.score = name, age, score
-        self.track = tracks
-        
-    def change_name(self,param):
-        self.name = param
-       
-    def change_age(self,param):
-        self.age = param 
-      
-    def add_track(self,param):
-        self.track.append(param)
-      
-    def get_score(self):
-        return self.score
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = str(tracks)
+        self.score = float(score)
 
+    def change_name(abc, name):
+        abc.name = name
+        return name
 
+    def change_age(abc, age):
+        abc.age = age
+        return age
 
-Bob = Student("Bob",26,["FE","BE"],20.90)
+    def add_tracks(abc, tracks):
+        abc.tracks = tracks
+        return tracks
+
+    def get_score(abc, score):
+        abc.score = score
+        return score
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score(self,param)
+Bob.change_name(str("Peter"))
+Bob.change_age(int(34))
+Bob.add_tracks(["UI/UX"])
+Bob.get_score(float(87))
 
+print(Bob.name)
+print(Bob.age)
+print(Bob.tracks)
+print(Bob.score)
 
